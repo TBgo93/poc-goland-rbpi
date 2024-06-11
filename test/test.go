58de23e02a82b293e509cbe552daa9fcb3e7cfd8
TBgo93 <tbaigorria93@gmail.com>
2024-06-10 21:31:57 -0300

    Test file

---
+++ test/test.go
@@ -0,0 +1,82 @@
+package test
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Memory struct {
+	MemTotal     int
+	MemFree      int
+	MemAvailable int
+}
+
+func ReadMemoryStats() Memory {
+	file, err := os.Open("/proc/meminfo")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	bufio.NewScanner(file)
+	scanner := bufio.NewScanner(file)
+	res := Memory{}
+	for scanner.Scan() {
+		key, value := parseLine(scanner.Text())
+		switch key {
+		case "MemTotal":
+			res.MemTotal = value
+		case "MemFree":
+			res.MemFree = value
+		case "MemAvailable":
+			res.MemAvailable = value
+		}
+	}
+	return res
+}
+
+func GetLocalIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddress := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddress.IP
+}
+
+// Revisar necesidad ---
+func parseLine(raw string) (key string, value int) {
+	text := strings.ReplaceAll(raw[:len(raw)-2], " ", "")
+	keyValue := strings.Split(text, ":")
+	return keyValue[0], toInt(keyValue[1])
+}
+
+func toInt(raw string) int {
+	if raw == "" {
+		return 0
+	}
+	res, err := strconv.Atoi(raw)
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
+
+func MainTest() {
+	mem := ReadMemoryStats()
+	freeRamPercentage := mem.MemAvailable * 100 / mem.MemTotal
+	parsedFreeRamPercentage := strconv.Itoa(freeRamPercentage) + "%"
+
+	fmt.Println("----------")
+	fmt.Println("Uso de RAM: " + parsedFreeRamPercentage)
+	fmt.Println("IP: ", GetLocalIP())
+	fmt.Println("----------")
+
+}
